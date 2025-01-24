98e67283e603e0c82026acc01d5f898334a18b72
Sleepingoff <justjh30@gmail.com>
2025-01-24 15:42:30 +0900

    Initial project setup

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import GlobalStyle from "./styles/GlobalStyle";
+
+function App() {
+  return (
+    <>
+      <GlobalStyle />
+      <div>
+        <h1>Welcome to Stardew Quiz!</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
